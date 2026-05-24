0a5bbad9de42310e34e450bba895a41b252f0cf5
devas-triadai <deva.s@triad-ai.com>
2026-05-24 14:08:43 +0530

    Fixes 1

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, Text, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, Text, ForeignKey, Enum, JSON
 from sqlalchemy.orm import relationship, backref
 from datetime import datetime
 import enum
